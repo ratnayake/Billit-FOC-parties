e69a9a93a647bdb06d5d0354d7e60b41da5bf57f
Isuru Sampath Ratnayake <isuru.sampath@ratnayake.info>
2025-05-09 19:00:11 +0530

    formatted
    code formatted to look nice

--- parties.php
+++ parties.php
@@ -1,20 +1,25 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
 
+$body = [
+    "CommercialName"    => "isurucomp",
+    "PartyType"         => "Customer",
+    "CompanyName"       => "Isuru Computers Pvt Ltd",
+    "Contact"           => "fff",
+    "ContactFirstName"  => "fff",
+    "ContactLastName"   => "fff"
+];
+
 $response = $client->request('POST', 'https://api.billit.be/v1/parties', [
-    'body' => '{"CommercialName":"isurucomp","PartyType":"Customer","CompanyName":"Isuru Computers Pvt Ltd",  "Contact": "fff",
-  "ContactFirstName": "fff",
-  "ContactLastName": "fff"}',
-  'headers' => [
-    'accept' => 'application/json',
-    'Content-Type' => 'application/json',
-    'apikey' => '2809bdb3-a58f-4cd5-9ba8-84cce4d1e476',
-  ],
+    'body' => json_encode($body),
+    'headers' => [
+        'Accept'        => 'application/json',
+        'Content-Type'  => 'application/json',
+        'apikey'        => '2809bdb3-a58f-4cd5-9ba8-84cce4d1e476',
+    ],
 ]);
 
 echo $response->getBody();
